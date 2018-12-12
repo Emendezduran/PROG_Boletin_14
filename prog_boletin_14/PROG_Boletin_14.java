0fe24d7fb5e1acf83c7511e9837140a9ac7e7960
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-12-12 12:18:10 +0100

    commit

--- prog_boletin_14/PROG_Boletin_14.java
+++ prog_boletin_14/PROG_Boletin_14.java
@@ -11,22 +11,19 @@ public class PROG_Boletin_14 {
     public static void main(String[] args) {
 
         ConversorTemperaturas c1 = new ConversorTemperaturas();
-
+        float temp;
+    do{
         System.out.println("Introduce la temperatura en centigrados a convertir");
         Scanner scan = new Scanner(System.in);
-        float temp = scan.nextFloat();
+        temp = scan.nextFloat();
 
         try {
             System.out.println("temperatura en Fharenheit: " + c1.centigradosAFharenheit(temp));
-        } catch (TemperaturaErradaExcepcion e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
             System.out.println("temperatura en Reamur: " + c1.centigradosAReamur(temp));
         } catch (TemperaturaErradaExcepcion e) {
             System.out.println(e.getMessage());
         }
+    }while(temp<c1.valorConstante);
 
     }
 
